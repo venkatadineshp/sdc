39e3d3812a84453d4ff09a04afa90f4afe73b17e
Dinesh <dinesh@DineshVarchuri2.home>
2014-10-25 21:05:44 -0400

    first commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Dinesh on 10/25/14.
+//  Copyright (c) 2014 Staples Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Dinesh on 10/25/14.
+//  Copyright (c) 2014 Staples Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
